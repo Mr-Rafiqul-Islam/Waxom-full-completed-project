eb707875cdfbe08e0a16db746325bc82c2192a62
mr-rafiqul-islam <rafiqulislamemon05@gmail.com>
2024-01-30 21:38:58 +0600

    4th update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-//  Sticky Header  
-$(window).scroll(function(){
-    var scrolling = $(this).scrollTop();
-        
-    if(scrolling > 600){
-            $('.navbar').addClass('sticky_header');
-    }
-        else {
-             $('.navbar').removeClass('sticky_header');
-    }
-    });  
+//  Sticky Header
+$(window).scroll(function () {
+  var scrolling = $(this).scrollTop();
+
+  if (scrolling > 600) {
+    $(".navbar").addClass("sticky_header");
+  } else {
+    $(".navbar").removeClass("sticky_header");
+  }
+});
+
+// for mixit up
+var containerEl = document.querySelector(".all_projects");
+
+var mixer = mixitup(containerEl);
